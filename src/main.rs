d9ee1fce37526311b4d410ce14cc1d118ef5a8f2
William Tuominiemi <william.m.tuominiemi@gmail.com>
2024-08-25 18:54:50 +0300

    get all directories and files, check if they include search_keyword

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
+use walkdir::WalkDir;
+
 use std::io::{self, Write};
 
 fn main() {
-    print!("Enter your search term: ");
+    print!("Please enter your keyword: ");
     io::stdout().flush().unwrap(); 
 
-    let mut name = String::new();
+    let mut search_keyword = String::new();
     io::stdin()
-        .read_line(&mut name)
+        .read_line(&mut search_keyword)
         .expect("Failed to read line");
 
-    let name = name.trim();
-    println!("Query: {}", name);
+    let search_keyword = search_keyword.trim(); 
+
+    let start_dir = "/";
+
+    for entry in WalkDir::new(start_dir).into_iter().filter_map(|e| e.ok()) {
+        let file_name = entry.file_name().to_string_lossy();
+
+        if file_name.contains(search_keyword) {
+            println!("{}", entry.path().display());
+        }
+    }
 }
