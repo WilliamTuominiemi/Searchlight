2a70cb02daef9b692c8e43810acbdd6e8c297266
William Tuominiemi <william.m.tuominiemi@gmail.com>
2024-08-25 17:37:38 +0300

    user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    print!("Enter your search term: ");
+    io::stdout().flush().unwrap(); 
+
+    let mut name = String::new();
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    let name = name.trim();
+    println!("Query: {}", name);
 }
